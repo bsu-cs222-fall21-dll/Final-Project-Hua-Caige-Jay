6ccccaa48da17ff2d942276718464dacac43b056
caige <caigerogers12@gmail.com>
2021-10-27 19:36:01 -0400

    fixes throughout program but buggy

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -1,15 +1,16 @@
+import java.io.IOException;
 import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Menu {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
         Teacher teacher = new Teacher();
         Students students = new Students();
         Grading grading  = new Grading();
         int quit = 0;
         String answer;
-        System.out.println("Welcome to the Zybra!!!");
+        System.out.println("Welcome to the Zybra ");
         while(quit == 0){
             System.out.println("Enter 1 for teacher, 2 for student, or q to quit");
             answer = scanner.nextLine();

2309362b97ce4f6218ea6a3d0ca1f0ff494b89a2
caige <caigerogers12@gmail.com>
2021-11-16 08:51:41 -0500

    push

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -10,7 +10,6 @@ public class Menu {
         String answer;
         System.out.println("Welcome to the Zybra!!!");
         label:
-
         while(quit == 0){
             System.out.println("Enter 1 for teacher, 2 for student, or q to quit");
             answer = scanner.nextLine();
